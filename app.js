edb5ebf67e8463a262884aab20604b277ab6adef
Nicolas Liu <94934469+Nicolasliu2@users.noreply.github.com>
2021-12-13 18:09:15 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,87 @@
+let allMovieData;
+
+document.addEventListener('DOMContentLoaded', async () => {
+    await getMovieData();
+    await movieOptionTags(allMovieData);
+
+    const selectTag = document.querySelector('select');
+
+    selectTag.onchange = () => {
+        for (let i = 0; i < allMovieData.length; i++) {
+            if (allMovieData[i].title === selectTag.value) {
+                selectedMovieDetails(allMovieData[i]);
+                return;
+            }
+        }
+    };
+
+    const submitButton = document.querySelector('#submit-btn')
+
+    submitButton.addEventListener('click', (event) => {
+        event.preventDefault();
+        const text = document.querySelector('#input-text');
+        if (selectTag.value.length === 0) {
+            return;
+        }
+        if (text.value.length) {
+            addListItem(text.value);
+            text.placeholder = 'Write a review';
+        } else {
+            text.placeholder = 'You cannot submit a empty review';
+        }
+        text.value= '';
+    })
+})
+
+const getMovieData = async () => {
+    let url = 'https://ghibliapi.herokuapp.com/films';
+    await fetch(url)
+        .then(respose => respose.json())
+        .then(data => {
+            allMovieData = data;
+        })
+}
+
+const movieOptionTags = (movieObj) => {
+    for (let i = 0; i < movieObj.length; i++) {
+        const optionTag = document.createElement('option');
+        optionTag.value = movieObj[i].title;
+        optionTag.innerText = movieObj[i].title;
+        document.querySelector('#select-movies').appendChild(optionTag);
+    }
+}
+
+const selectedMovieDetails = (selectedMovie) => {
+    const movieDetailTag = document.querySelector('#movie-details');
+
+    movieDetailTag.innerHTML = '';
+
+    const title = document.createElement('h3');
+    const year = document.createElement('p');
+    const movieimg = document.createElement('p');
+    const description = document.createElement('p');
+    description.classList.add('movie-description');
+    const moviedirector = document.createElement('p');
+
+
+    title.innerText = selectedMovie.title;
+    year.innerText = selectedMovie.release_date;
+    movieimg.innerHTML = `<img src="${selectedMovie.image}"/>`;
+    movieimg.classList.add('movie-img');
+    description.innerText = selectedMovie.description;
+    moviedirector.innerText = "Director: " + selectedMovie.director;
+    
+    movieDetailTag.appendChild(title)
+    movieDetailTag.appendChild(year)
+    movieDetailTag.appendChild(movieimg)
+    movieDetailTag.appendChild(description)
+    movieDetailTag.appendChild(moviedirector)
+}
+
+const addListItem = (text) => {
+    const li = document.createElement('li');
+    li.classList.add('li-text')
+    const movieTitle = document.querySelector('select').value;
+    li.innerText = movieTitle + ": " + text;
+    document.querySelector('ul').appendChild(li);
+}
